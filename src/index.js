038a70872d4c541ec84daeff1eb5f530059e153a
Ruairí Conway <32017481+ruairiconway@users.noreply.github.com>
2021-08-20 13:06:13 -0400

    add and remove li implemented
    Need to add in an id object value for each.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import { Card, TextInput } from '@contentful/forma-36-react-components';
+import { List, ListItem, Card, TextInput, FormLabel, Button } from '@contentful/forma-36-react-components';
 import { init } from 'contentful-ui-extensions-sdk';
 import '@contentful/forma-36-react-components/dist/styles.css';
 import './index.css';
@@ -12,34 +12,33 @@ export const App = ({sdk}) => {
     {
       listArr: [
         {
-          header: "header1", content: "content1"
+          header: "",
+          content: ""
         }
       ],
       dragging: false
     }
   )
 
-  const onExternalChange = value => {
-    setListItems(value);
-  }
+  // const onExternalChange = value => {
+  //   setListItems(value);
+  // }
 
   const handleChange = (e) => {
     const target = e.target
     const targetIndex = target.dataset.index
-
     const newList = listItems.listArr.slice()
-    if (target.name === "header") { newList[targetIndex] = {
-        header: target.value,
-        content: newList[targetIndex].content
-      }
+    if (target.name === "header") {
+      newList[targetIndex].header = target.value
     } else if (target.name === "content") {
-      newList[targetIndex] = {
-        header: newList[targetIndex].header,
-        content: target.value
-      }
+      newList[targetIndex].content = target.value
     }
-    console.log(newList)
-    setListItems({listArr: newList})
+    setListItems( prevState => (
+      {
+        ...prevState,
+        listArr: newList
+      }
+    ))
     sdk.field.setValue(listItems)
   }
 
@@ -47,34 +46,77 @@ export const App = ({sdk}) => {
     sdk.window.startAutoResizer();
   }, []);
 
-  useEffect(() => {
-    // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
-    const detatchValueChangeHandler = sdk.field.onValueChanged(onExternalChange);
-    return detatchValueChangeHandler;
-  });
+  // useEffect(() => {
+  //   // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
+  //   const detatchValueChangeHandler = sdk.field.onValueChanged(onExternalChange);
+  //   return detatchValueChangeHandler;
+  // });
+
+
+  const handleAddItem = () => {
+    setListItems( prevState => (
+      {
+        ...prevState,
+        listArr: [
+          ...prevState.listArr,
+          {
+            header: "",
+            content: ""
+          }
+        ]
+      }
+    ))
+    sdk.field.setValue(listItems)
+  }
+
+  const handleDeleteItem = (e) => {
+    const target = e.target
+    const targetIndex = target.closest(".btn-remove").dataset.index
+    const newList = listItems.listArr.slice()
+    for (let i = 0; i < newList.length; i++) {
+      if (i == targetIndex) {
+        newList.splice(targetIndex, 1)
+      }
+    }
+    setListItems( prevState => (
+      {
+        ...prevState,
+        listArr: newList
+      }
+    ))
+    sdk.field.setValue(listItems)
+  }
+
+  console.log(listItems)
 
   return (
     <>
+    <List element="ol">
       { listItems.listArr.map( (item, i) => (
-        <Card
-          key={i}
-        >
-          <TextInput
-            type="text"
-            name="header"
-            value={item.header}
-            onChange={handleChange}
-            data-index={i}
-          />
-          <TextInput
-            type="text"
-            name="content"
-            value={item.content}
-            onChange={handleChange}
-            data-index={i}
-          />
-        </Card>
+        <ListItem key={i}>
+          <Card>
+            <FormLabel htmlFor="header">Heading</FormLabel>
+            <TextInput
+              type="text"
+              name="header"
+              value={item.header}
+              onChange={handleChange}
+              data-index={i}
+            />
+            <FormLabel htmlFor="content">Content</FormLabel>
+            <TextInput
+              type="text"
+              name="content"
+              value={item.content}
+              onChange={handleChange}
+              data-index={i}
+            />
+            <Button buttonType="negative" size="small" icon="HorizontalRule" data-index={i} onClick={handleDeleteItem} className="btn-remove" ></Button>
+          </Card>
+        </ListItem>
       ))}
+    </List>
+    <Button buttonType="positive" size="small" icon="Plus" onClick={handleAddItem} ></Button>
     </>
   )
 }
